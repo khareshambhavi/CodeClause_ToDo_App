e8b380e271395b6f994f72b57ba1601f182947fa
Shambhavi <shambhavikhare.mahak@gmail.com>
2023-07-31 14:29:54 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/todo_codeclause/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_codeclause
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+const Color tRed = Color(0xffE0115F);
+const Color tBlue = Color(0xFF6495ED);
+
+const Color tBlack = Color(0xFF3A3A3A);
+const Color tGrey = Color(0xFF717171);
+
+const Color tBGColor = Color(0xFFEEEFF5);
